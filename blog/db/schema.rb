8aa3bfae4f08daf135b4fa276b2ef5c9a51e2416
dcmspe <dcms.pe@gmail.com>
2015-05-26 14:27:57 -0300

    6.4 Genereting a Controller

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526124101) do
+ActiveRecord::Schema.define(version: 20150526170935) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20150526124101) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "coments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "coments", ["article_id"], name: "index_coments_on_article_id"
 
 end
